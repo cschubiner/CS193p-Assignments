2b35b8f952d9931185b272bc9e25308f51862414
Clay Schubiner <cschubiner@gmail.com>
2014-04-04 19:59:28 -0700

    hi

--- Matchismo/Model/Card.m
+++ Matchismo/Model/Card.m
@@ -12,15 +12,12 @@
 - (int)match:(NSArray *)otherCards
 {
     int score = 0;
-    
     for (Card *card in otherCards) {
         if ([card.contents isEqualToString:self.contents]) {
             score = 1;
         }
     }
-    
     return score;
 }
-
 
 @end
