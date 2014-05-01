bd52009470a261b7c89f48b5d2773a70a774af27
Clay Schubiner <cschubiner@gmail.com>
2014-05-01 02:48:01 -0700

    hi

--- Matchismo/CardView.m
+++ Matchismo/CardView.m
@@ -13,16 +13,6 @@
 -(void)updateWithCard:(Card *)card {
 }
 
-/*
- // Only override drawRect: if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- - (void)drawRect:(CGRect)rect
- {
- // Drawing code
- }
- */
-
-
 #pragma mark Initialization
 
 - (void)setup
